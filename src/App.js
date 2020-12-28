43a94c9d9a64677e1206f415db4bfbc5ac4388f0
silviapio <silvia.piovesan@gmail.com>
2020-12-28 11:48:13 +0100

    exercise level 2

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </header>
-        <Salutacio />
+        <Salutacio name="Montse"/>
       </div>
     );
   }
