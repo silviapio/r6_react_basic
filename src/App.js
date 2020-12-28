2f8992c72bde1aac4be5420c26167866085009ab
silviapio <silvia.piovesan@gmail.com>
2020-12-28 12:16:39 +0100

    exercise 4 level 3

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  
+  handleClick = name =>  alert(`You look beautiful today, ${name}!`);
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </header>
-        <Salutacio name="Montse"/>
+        <Salutacio name="Montse" handleClick={this.handleClick}/>
       </div>
     );
   }
--- src/Salutacio.js
+++ src/Salutacio.js
@@ -1,9 +1,17 @@
 import React, {Component} from "react";
 
 class Salutacio extends Component {
+
+    handleClick = () => this.props.handleClick(this.props.name);
+
     render(){
         return(
+            <div>
             <h1>Hello {this.props.name}!</h1>
+            <button onClick={this.handleClick}>
+                Click me!
+            </button>
+            </div>
         )
     }
 }
